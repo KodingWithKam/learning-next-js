929d586f0a370f668dad9a2c17facdf2b35c30ee
KamHaze <kodingwithkam@gmail.com>
2024-12-23 14:17:13 -0500

    parallel routes

---
+++ src/app/@login/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Login Page!</h1>
+      <button>Get in the APP!</button>
+    </div>
+  );
+}
--- src/app/@team/page.tsx
+++ src/app/@team/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <>Hello Page!</>;
+  return <div>Team Page!</div>;
 }
