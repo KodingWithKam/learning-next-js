c0456280d9c3a3f9d232ddbb9b430e63324da291
KamHaze <kodingwithkam@gmail.com>
2024-12-29 15:38:19 -0500

    intercepting login route

---
+++ src/app/@auth/(.)login/page.tsx
@@ -0,0 +1,10 @@
+import Modal from '@/app/_components/Modal/Modal';
+import LoginForm from '@/app/_components/Login/LoginForm/LoginForm';
+
+export default function Page() {
+  return (
+    <Modal>
+      <LoginForm />
+    </Modal>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/app/_components/Login/LoginForm/LoginForm';
+
+export default function Page() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
