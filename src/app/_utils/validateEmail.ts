cfa38ae55c7ac102b6b7f4296dcc1d7e11aebce4
KamHaze <kodingwithkam@gmail.com>
2025-01-17 18:53:59 -0500

    fix typo

--- src/app/_utils/validateEmail.ts
+++ src/app/_utils/validateEmail.ts
@@ -3,7 +3,7 @@
  * @param email
  * @constructor
  */
-export default function ValidateEmail(email: string) {
+export default function validateEmail(email: string) {
   if (!email) return false;
   const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
   return emailRegex.test(email);
